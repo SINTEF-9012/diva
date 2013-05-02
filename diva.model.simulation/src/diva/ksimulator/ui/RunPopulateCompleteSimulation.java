39310d09e4bca13c885670d33be8a16d8e1ebb0a
brice-morin <brice.morin1@gmail.com>
2013-05-02 13:43:23 +0200

    spring clean for the May Queen

--- diva.model.simulation/src/diva/ksimulator/ui/RunPopulateCompleteSimulation.java
+++ diva.model.simulation/src/diva/ksimulator/ui/RunPopulateCompleteSimulation.java
@@ -1,5 +1,6 @@
 package diva.ksimulator.ui;
 
+import java.io.File;
 import java.util.Iterator;
 
 import org.eclipse.core.resources.IFile;
@@ -10,46 +11,42 @@ import org.eclipse.ui.IActionDelegate;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 
-/*import diva.ksimulator.KExecMain;
-
-import fr.irisa.triskell.eclipse.console.EclipseConsole;
-import fr.irisa.triskell.eclipse.console.IOConsole;
-import fr.irisa.triskell.eclipse.console.messages.ErrorMessage;
-import fr.irisa.triskell.eclipse.console.messages.InfoMessage;
-import fr.irisa.triskell.eclipse.console.messages.OKMessage;
-import fr.irisa.triskell.eclipse.console.messages.ThrowableMessage;*/
+import diva.VariabilityModel;
+import diva.helpers.DivaHelper;
 
 public class RunPopulateCompleteSimulation implements IObjectActionDelegate, Runnable {
 
 	protected StructuredSelection currentSelection;
-    protected IFile file;
-	
+	protected IFile file;
+
 	public RunPopulateCompleteSimulation() {
 		super();
 	}
-	
+
 	public void run() {
+
+		String file_uri = file.getLocation().toOSString();
 		
-		/*IOConsole console = new EclipseConsole("DiVA Simulator");
-		console.println(new InfoMessage("Launching DiVA Simulator on file : " + file.getLocation().toOSString() + "..."));*/
-		
-		System.out.println("Launching DiVA Simulator on file : " + file.getLocation().toOSString() + "...");
+		System.out.println("Launching DiVA Simulator on file : " + file_uri + "...");
+		long start = System.currentTimeMillis();
 		
 		try {			
 				
-			String file_uri = "file:/" + file.getLocation().toOSString();
-		    
-			//TODO: call populateCompleteSimulation
-			/*KExecMain.run("populateCompleteSimulation", file_uri, console);
+			VariabilityModel model = DivaHelper.load(new File(file_uri));
 			
-			console.println(new OKMessage("Execution terminated successfully."));*/
+			if (model.getSimulation() != null) {
+				DivaHelper.computeSuitableConfigurations(model);
+				model.getSimulation().populatePriorities();
+				model.getSimulation().populateScores();
+				model.getSimulation().populateVerdicts();
+			}
 			
-			System.out.println("Execution terminated successfully.");
+			DivaHelper.save(model, file_uri);
+			
+			System.out.println("Execution terminated successfully. Took: " + (System.currentTimeMillis() - start) + " ms");	
 			
 			
 		} catch (Throwable e) {
-			/*console.println(new ErrorMessage("Runtime error : "));
-			console.println(new ThrowableMessage(e));*/
 			e.printStackTrace();
 		}
 	}
@@ -71,7 +68,7 @@ public class RunPopulateCompleteSimulation implements IObjectActionDelegate, Run
 	 * @see IActionDelegate#selectionChanged(IAction, ISelection)
 	 */
 	public void selectionChanged(IAction action, ISelection selection) {
-		
+
 		if (selection instanceof StructuredSelection)
 		{
 			currentSelection = (StructuredSelection)selection;
@@ -82,7 +79,7 @@ public class RunPopulateCompleteSimulation implements IObjectActionDelegate, Run
 			}
 
 		}
-		
+
 	}
 
 }
